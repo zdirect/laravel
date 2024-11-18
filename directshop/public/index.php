064d6a919969386a15c5bc8ad5054949b348a7e6
ostopenchuk <ostopenchuk@eliteit.org>
2024-11-18 16:36:06 +0200

    Update index.php

--- directshop/public/index.php
+++ directshop/public/index.php
@@ -19,8 +19,8 @@ define('LARAVEL_START', microtime(true));
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
-echo 'test1';
-echo 'test2';
+die();
+echo 'tetsetest';
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
